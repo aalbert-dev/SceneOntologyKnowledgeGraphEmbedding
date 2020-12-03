124b5836348f38b347fe6518fc2f17e3bb2f7e65
Arjun Albert <34843324+aalbert-dev@users.noreply.github.com>
2020-12-03 00:15:32 -0500

    Add NuScenes dataset readers (old)

---
+++ ads_kge.cpp
@@ -0,0 +1,100 @@
+#include "ads_kge.hpp"
+
+/**
+ * author   Arjun Albert
+ * email    aalbert@mit.edu
+ * date     11/26/2020
+**/
+
+/*
+ * Read in json file through input stream with a given file name relative to program location.
+ */ 
+json read_json(std::string file_name)
+{
+    std::ifstream file_in(file_name);
+    json json_out;
+    file_in >> json_out;
+    return json_out;
+}
+
+/*
+ * Parse the scene description from the json element.
+ * Returns the scene description as a string.
+ */
+std::string get_scene_description(json element)
+{
+    std::string element_description;
+    element_description = element["description"];
+    return element_description;
+}
+
+
+/*
+ * Parse a scene description into individual objects. 
+ * Returns the scene objects as a vector of strings. 
+ */
+std::vector<std::string> get_objects_from_description(std::string description)
+{
+    std::string delimiter = ",";
+    std::vector<std::string> objects;
+
+    size_t pos = 0;
+    std::string token;
+    while ((pos = description.find(delimiter)) != std::string::npos) {
+        token = description.substr(0, pos);
+        objects.push_back(token);
+        description.erase(0, pos + delimiter.length());
+    }
+    return objects;
+}
+
+
+/*
+ * Check if a scene entity already exists as a node in the graph.
+ * Returns true if the entity already exists, returns false otherwise.
+ */ 
+bool is_duplicate_entity(std::string object)
+{
+    for (auto& element : entities) {
+        if (element.compare(object) == 0){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+/*
+ * Main program loop.
+ * Reads in scene json data, adds entities and relations to an adjacency matrix graph representation.
+ * Then embeds the knowledge graph using the translating embedding model for multi-relational data.
+ */
+int main()
+{
+    
+    json j = read_json(scene_data_file_name);
+    
+    for (auto& element : j) {
+        std::string description = get_scene_description(element);
+        descriptions.push_back(description);
+    }
+    
+
+    for (auto& description : descriptions) {
+        std::vector<std::string> objects = get_objects_from_description(description);
+        multi_level_descriptions.push_back(objects);
+        for (auto& object : objects){
+            bool result = is_duplicate_entity(object);
+            if (!result){
+                entities.push_back(object);
+            }
+        }
+    }
+
+    define_relationships(); 
+
+    labelledAdjacencyGraph non_embedded_graph = form_knowledge_graph();
+
+    labelledAdjacencyGraph embedded_graph = embed_knowledge_graph(non_embedded_graph);
+    return 1;
+}
---
+++ ads_kge.hpp
@@ -0,0 +1,174 @@
+#include "json/single_include/nlohmann/json.hpp"
+#include <fstream>
+#include <iostream>
+
+/**
+ * author   Arjun Albert
+ * email    aalbert@mit.edu
+ * date     11/26/2020
+**/
+
+// json namespace
+using json = nlohmann::json;
+
+// use the scene.json file as data
+std::string scene_data_file_name = "scene.json";
+
+// store all the scene descriptions
+std::vector<std::string> descriptions;
+
+// store the scene descriptions in ordered form
+std::vector<std::vector<std::string>> multi_level_descriptions;
+
+// store the scene entities as strings
+std::vector<std::string> entities;
+
+// store the scene relationships as strings
+std::vector<std::string> relationships;
+
+// define possible entity relations
+std::string all_entities[] {"with", "before", "after", "none", "self"};
+
+
+// define custom structure to hold entities and relations for an adjacency matrix graph representation knowledge graph embedding
+struct labelledAdjacencyGraph{
+    std::string name;
+    std::vector<std::string> headers;
+    std::vector<std::vector<std::string>> adjacency_matrix;
+    std::vector<std::string> values;
+};
+
+
+/*
+ * Add all relationships to relationship data structure.
+ */
+inline void define_relationships()
+{
+    for (std::string entity: all_entities){
+        relationships.push_back(entity);
+    }
+}
+
+
+/*
+ * Get the level of an entity based on the order of the entity in the structure.
+ * Returns the entity level or -1 if it does not exist in the structure. 
+ */
+inline int get_entity_level(std::string target_entity)
+{
+    int i = 0;
+    for (i; i < descriptions.size(); i++){
+        for (std::string entity: multi_level_descriptions[i]){
+            if (target_entity.compare(entity) == 0){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+
+/*
+ * Find the relationship between two entities based on their level in the structure.
+ * Also check if the entities are the same or not related. 
+ * Returns the relationship between entity 1 and entity 2.
+ */
+inline std::string get_relationship(std::string entity_1, std::string entity_2)
+{
+    int entity_1_level = get_entity_level(entity_1);
+    int entity_2_level = get_entity_level(entity_2);
+
+    if (entity_1.compare(entity_2) == 0){
+        return all_entities[4];
+    }
+    if (entity_1_level < 0 || entity_2_level < 0){
+        return all_entities[3];
+    }
+
+    if (entity_1_level == entity_2_level){
+        return all_entities[0];
+    }else if (entity_1_level < entity_2_level){
+        return all_entities[1];
+    }else{
+        return all_entities[2];
+    }
+}
+
+
+/*
+ * Calculate the adjacency matrix graph representation by finding the relationships between
+ * every entity in the scene.
+ * The adjacency matrix is a   
+ */
+inline std::vector<std::vector<std::string>> calculate_adjacency_matrix(){
+    std::vector<std::vector<std::string>> adjacency_matrix;
+    for (std::string s1: entities){
+        std::vector<std::string> matrix_row;
+        for (std::string s2: entities){
+            std::string relation = get_relationship(s1, s2);
+            matrix_row.push_back(relation);
+        }
+        adjacency_matrix.push_back(matrix_row);
+    }
+    return adjacency_matrix;
+}
+
+
+/*
+ * Add entities and relations between entities to knowledge graph adjacency matrix representation using'
+ * the defined labelledAdjacencyGraph structure.
+ * Returns the labelledAdjacencyGraph with entities and relations. 
+ */
+inline labelledAdjacencyGraph form_knowledge_graph()
+{
+    labelledAdjacencyGraph non_embedded_knowledge_graph;
+    non_embedded_knowledge_graph.name = "Non embedded";
+    non_embedded_knowledge_graph.headers = entities;
+    for (std::string value: all_entities){
+        non_embedded_knowledge_graph.values.push_back(value);
+    }
+    non_embedded_knowledge_graph.adjacency_matrix = calculate_adjacency_matrix();
+    return non_embedded_knowledge_graph;
+}
+
+
+inline float score_entity_with_relation(labelledAdjacencyGraph graph, std::string entity, std::string relation)
+{
+    return 0.0;
+}
+
+
+inline float score_relationship(labelledAdjacencyGraph graph, std::string entity_1, std::string entity_2)
+{
+    std::string relation = get_relationship(entity_1, entity_2);
+    return 0.0;
+}
+
+
+inline labelledAdjacencyGraph embed_knowledge_graph(labelledAdjacencyGraph graph)
+{
+    labelledAdjacencyGraph embedded_knowledge_graph;
+    return embedded_knowledge_graph;
+}
+
+
+inline void calculate_embedding(std::string entity)
+{
+    float margin = 0;
+
+    float pos_h_e = 0;
+    float pos_t_e = 0;
+    float pos_r_e = 0;
+
+    float neg_h_e = 0;
+    float neg_t_e = 0;
+    float neg_r_e = 0;
+
+    float pos_norm = 0;
+    float neg_norm = 0;
+
+    float pos = pow(pos_h_e + pos_r_e - pos_t_e, 2);
+    float neg = pow(neg_h_e + neg_r_e - neg_t_e, 2);
+
+    float loss = std::max(pos - neg + margin, (float) 0.0);
+}
